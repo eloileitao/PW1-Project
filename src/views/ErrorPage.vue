a3b2dcb693b6de5b92d30a5d6a3e80cc2f7c0368
joao silva <joao.ppsilva.98@gmail.com>
2020-01-30 17:25:26 +0000

    Error Pages Done

---
+++ src/views/ErrorPage.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <h1>Não foi possivel aceder a esta rota!</h1>
+  <router-link :to="{name:'home'}" tag="button">
+    <v-btn class="mb-1 ml-5" fab dark large color="purple">
+      <v-icon dark>mdi-home</v-icon>
+    </v-btn>
+  </router-link>
+  </div>
+</template>
+
+<script>
+</script>
---
+++ src/views/ErrorPageLoggedUser.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+  <h1>Não tem autorização para aceder a esta página!</h1>
+  <router-link :to="{name:'home'}" tag="button">
+    <v-btn class="mb-1 ml-5" fab dark large color="purple">
+      <v-icon dark>mdi-home</v-icon>
+    </v-btn>
+  </router-link>
+  </div>
+</template>
