594d3e3a4e2f6d10be2330561ee30fce81ad2a60
joao silva <joao.ppsilva.98@gmail.com>
2020-01-30 17:26:41 +0000

    Fixed Error

--- src/views/ErrorPageLoggedUser.vue
+++ src/views/ErrorPageLoggedUser.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <h1>Não tem autorização para aceder a esta página!</h1>
-  <router-link :to="{name:'home'}" tag="button">
+  <router-link :to="{name:'login'}" tag="button">
     <v-btn class="mb-1 ml-5" fab dark large color="purple">
       <v-icon dark>mdi-home</v-icon>
     </v-btn>
